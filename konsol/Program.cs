703c1353e35e0c59ac7004c41b9ab036ca59ba3a
mirkelam32 <mirkelam1299@gmail.com>
2020-04-18 07:22:37 +0300

    ilk sürüm

---
+++ konsol/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konsol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+              
+              
+            
+
+            string ad;//soru : Merhaba benim adım Yavuz. Senin adın ney ? -xxxx
+            Console.WriteLine(" merhaba benim adım Abdullah. Senin adın ne? ");
+            ad = Console.ReadLine();
+
+            Console.WriteLine(" Memnun oldum " + ad+ " nasılsın bugün " + ad);
+            Console.ReadLine(); //memnun oldum. Nasılsın bugün xxxx? 
+
+            Console.WriteLine("Bende iyiyim " + ad);
+            Console.ReadLine(); //İyiyim xxxx 
+
+
+
+
+
+        }
+    }
+}
